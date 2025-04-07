3d1f7aa04910bad9925b13b1c065d45978b53ced
Mia <miasiamskaya@yandex.ru>
2025-04-07 19:24:20 +0300

    feat: add initial solution

---
+++ ekaterina-anisimova-kt-42-22-April-25/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ekaterina_anisimova_kt_42_22_April_25
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
